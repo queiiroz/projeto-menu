c2669454a9ae4dab97223c42d175157fbe40c422
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2024-02-20 17:51:02 -0300

    add card-pratos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Hashtaurante from "./assets/hashtaurante.webp";
 import "./App.css";
 import { Navegacao } from "./Navegacao.jsx";
+import { ItemCardapio } from "./ItemCardapio.jsx";
 
 export function App() {
   return (
     <>
       <img src={Hashtaurante} alt="" className="capa" />
       <Navegacao />
+      <div className="menu">
+        <ItemCardapio />
+        <ItemCardapio />
+        <ItemCardapio />
+        <ItemCardapio />
+      </div>
     </>
   );
 }
