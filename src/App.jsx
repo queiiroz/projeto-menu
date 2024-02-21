1997d3d4733faf40e55d0932761ee416278c80bc
Gleidson Queiroz <gldsnqueiroz2@gmail.com>
2024-02-21 19:25:47 -0300

    nav funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
 import Hashtaurante from "./assets/hashtaurante.webp";
 import "./App.css";
 import { Navegacao } from "./Navegacao.jsx";
 import { ItemCardapio } from "./ItemCardapio.jsx";
 import { pratosPrincipais, sobremesas, bebidas } from "./cardapio.js";
 
+
 export function App() {
+
+  const paginasMenu = [pratosPrincipais, sobremesas, bebidas]
+  const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0);
+
   return (
     <>
       <img src={Hashtaurante} alt="" className="capa" />
-      <Navegacao />
+      <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
       <div className="menu">
-        {pratosPrincipais.map((item) => (
+        {paginasMenu[paginaSelecionada].map((item) => (
           <ItemCardapio
             nome={item.nome}
             descricao={item.descricao}
